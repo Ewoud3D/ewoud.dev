3728b71bbd61efa0edb682fb5b1d97dbc9df702b
Ewoud <github@ewoudva.nderwerf.com>
2025-04-12 18:01:59 +0200

    Moved files from static to assets folder

---
+++ themes/meindert/assets/js/game-library.js
@@ -0,0 +1,57 @@
+var filterAll = document.getElementById("filter-all");
+var filterOriginal = document.getElementById("filter-original");
+var filterContributed = document.getElementById("filter-contributed");
+
+var games = document.getElementsByClassName("game-cover");
+
+const activeClassName = "btn btn-primary";
+const defaultClassName = "btn btn-outline-primary";
+
+const allGames = "all";
+const originalGames = "original";
+const contributedGames = "contributed";
+
+filterAll.onclick = function() 
+{
+	filterAll.className = activeClassName;
+	filterOriginal.className = filterContributed.className = defaultClassName;
+
+	filterGame(allGames);
+};
+
+filterOriginal.onclick = function() 
+{	
+	filterOriginal.className = activeClassName;
+	filterContributed.className = filterAll.className = defaultClassName;
+
+	filterGame(originalGames);
+};
+
+filterContributed.onclick = function() 
+{
+	filterContributed.className = activeClassName;
+	filterAll.className = filterOriginal.className = defaultClassName;
+
+	filterGame(contributedGames);
+};
+
+function filterGame(filter)
+{
+	for (let i = 0; i < games.length; i++) {
+		const game = games[i];
+
+		if (game.dataset.gameType == filter || filter == allGames)
+			game.style.display = "block";
+		else
+			game.style.display = "none";
+	}
+}
+
+
+window.addEventListener("load", addAnimations, true);
+function addAnimations()
+{
+    const cardIcons = document.getElementsByClassName("game-cover");
+    for (let i = 0; i < cardIcons.length; i++)
+        cardIcons[i].classList.add("game-cover-animation");
+}
